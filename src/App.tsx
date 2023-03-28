ff23f6874f54832641c5a41577daa565c9678d53
Charlebois Christophe <dianketohead@gmail.com>
2023-03-28 17:44:00 +0200

    add page importation and start Homepage integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import 'bulma/css/bulma.min.css';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Homepage from './page/Homepage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
--- src/page/Homepage/index.tsx
+++ src/page/Homepage/index.tsx
@@ -2,7 +2,11 @@ import 'bulma/css/bulma.min.css';
 import React from "react";
 
 const Homepage = () => {
-
+    return (
+        <React.Fragment>
+            Homepage
+        </React.Fragment>
+    )
 }
 
 export default Homepage;
