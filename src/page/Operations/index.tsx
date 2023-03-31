bf1761b9754710c321f12671d0ec9bfc980ae2f4
Charlebois Christophe <dianketohead@gmail.com>
2023-03-31 12:19:43 +0200

    add Operations page

--- src/page/Operations/index.tsx
+++ src/page/Operations/index.tsx
@@ -2,7 +2,7 @@ import 'bulma/css/bulma.min.css';
 import React from "react";
 import styled from 'styled-components'
 import Header from '../../components/Header';
-import TransactionList from '../../components/TransactionList';
+import TransactionTable from '../../components/TransactionTable';
 
 const Title = styled.h1`
     display: flex;
@@ -12,11 +12,11 @@ const Title = styled.h1`
 `
 
 const transactions = [
-    { id: 1, date: '2022-01-01', description: 'Paycheck', amount: 5000 },
-    { id: 2, date: '2022-01-05', description: 'Rent', amount: -1000 },
-    { id: 3, date: '2022-01-10', description: 'Groceries', amount: -200 },
-    { id: 4, date: '2022-01-15', description: 'Dinner with friends', amount: -100 },
-    { id: 5, date: '2022-01-20', description: 'Gas', amount: -50 },
+    { id: 1, date: '2022-01-01', wording: 'Paycheck', amount: 5000 },
+    { id: 2, date: '2022-01-05', wording: 'Rent', amount: -1000 },
+    { id: 3, date: '2022-01-10', wording: 'Groceries', amount: -200 },
+    { id: 4, date: '2022-01-15', wording: 'Dinner with friends', amount: -100 },
+    { id: 5, date: '2022-01-20', wording: 'Gas', amount: -50 },
   ];
 
 
@@ -25,7 +25,7 @@ const Operations = () => {
         <React.Fragment>
             <Header />
             <Title><b>Opérations</b></Title>
-            <TransactionList transactions={transactions}/>
+            <TransactionTable transactions={transactions}/>
         </React.Fragment>
     )
 }
